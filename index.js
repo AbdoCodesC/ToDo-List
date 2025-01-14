4524912240dd3e6cd871cb5f27650165feee952a
AbdoCodesC <abdeerbusiness@gmail.com>
2025-01-13 20:36:58 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect()
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB failed", err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
